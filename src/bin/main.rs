570261f451d6b60338ce6b097ed277e72e83a35e
Matthew Mayer <matthewkmayer@gmail.com>
2015-07-31 13:42:01 -0700

    Working on file based credential sourcing.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -11,52 +11,54 @@ use time::*;
 
 fn main() {
 	if let Ok(creds) = creds_from_env() {
-		match sqs_roundtrip_tests(creds) {
-			Ok(_) => { println!("Everything worked."); },
-			Err(err) => { println!("Got error: {:#?}", err); }
-		}
-		/*match sns_roundtrip_tests(creds) {
-			Ok(_) => { println!("Everything worked."); },
-			Err(err) => { println!("Got error: {:#?}", err); }
-		}*/			
+		// match sqs_roundtrip_tests(creds) {
+		// 	Ok(_) => { println!("Everything worked."); },
+		// 	Err(err) => { println!("Got error: {:#?}", err); }
+		// }
+		let mah_key = creds.get_aws_access_key_id();
+		println!("creds: {}, {}", mah_key, mah_key);
+
 	} else {
-		println!("Set AWS credentials with environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_KEY");
-	}	
+		println!("Couldn't find AWS environment vars: Set AWS credentials with environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_KEY");
+	}
 }
 /*
 fn sns_roundtrip_tests(creds: AWSCredentials) -> Result<(), AWSError> {
 	let sns = SNSClient::new(creds, "us-east-1");
-	
+
 	let response = try!(sns.list_topics());
 	println!("{:#?}", response);
 	Ok(())
 }
 */
 fn sqs_roundtrip_tests(creds: AWSCredentials) -> Result<(), AWSError> {
-	
+
+	// Matthew's debugging fun
+	return Ok(());
+
 	let sqs = SQSHelper::new(&creds, "us-east-1");
-	
+
 	// list existing queues
-	let response = try!(sqs.list_queues());	
+	let response = try!(sqs.list_queues());
 	for q in response.queue_urls {
 		println!("Existing queue: {}", q);
 	}
-	
+
 	// create a new queue
 	let q_name = &format!("test_q_{}", get_time().sec);
 	let response = try!(sqs.create_queue(q_name));
 	println!("Created queue {} with url {}", q_name, response.queue_url);
-	
+
 	// query it by name
 	let response = try!(sqs.get_queue_url(q_name));
 	let queue_url = &response.queue_url;
 	println!("Verified queue url {} for queue name {}", queue_url, q_name);
-	
+
 	// send it a message
 	let msg_str = "lorem ipsum dolor sit amet";
 	let response = try!(sqs.send_message(queue_url, msg_str));
 	println!("Send message with body '{}' and created message_id {}", msg_str, response.message_id);
-	
+
 	// receive a message
 	let response = try!(sqs.receive_message(queue_url));
 	for msg in response.messages {
@@ -75,5 +77,5 @@ fn creds_from_env() -> Result<AWSCredentials, VarError> {
 	let env_key = try!(var("AWS_ACCESS_KEY_ID"));
 	let env_secret = try!(var("AWS_SECRET_KEY"));
 
-	Ok(AWSCredentials::new(&env_key, &env_secret)) 
+	Ok(AWSCredentials::new(&env_key, &env_secret))
 }
--- src/credentials.rs
+++ src/credentials.rs
@@ -1,5 +1,9 @@
 extern crate regex;
 use std::env::*;
+use std::fs::File;
+use std::path::Path;
+use std::error::Error;
+use std::io::prelude::*;
 
 #[derive(Clone, Debug)]
 pub struct AWSCredentials {
@@ -14,11 +18,11 @@ impl AWSCredentials {
             secret: secret.to_string()
         }
     }
-    
+
     pub fn get_aws_access_key_id(&self) -> &str {
     	&self.key
     }
-    
+
     pub fn get_aws_secret_key(&self) -> &str {
     	&self.secret
     }
@@ -31,8 +35,6 @@ pub trait AWSCredentialsProvider {
 
 pub struct EnvironmentCredentialsProvider;
 
-
-
 pub struct DefaultAWSCredentialsProviderChain {
 	credentials: AWSCredentials
 }
@@ -44,43 +46,57 @@ impl AWSCredentialsProvider for DefaultAWSCredentialsProviderChain {
 			self.credentials = env_creds.unwrap();
 			return;
 		}
-		
+
 		//let file_creds = DefaultAWSCredentialsProviderChain::creds_from_profile();
 	}
-	
-	
+
+
 	fn get_credentials(&self) -> &AWSCredentials {
 		&self.credentials
 	}
 
 }
 
-//struct ProfileCredentialsError;
+struct ProfileCredentialsError;
 
+// From http://blogs.aws.amazon.com/security/post/Tx3D6U6WSFGOK2H/A-New-and-Standardized-Way-to-Manage-Credentials-in-the-AWS-SDKs
+// 1. environment variables
+// 2. central credentials file (named profile is supplied, otherwise default)
+// 3. IAM role (if running on an EC2 instance)
 impl DefaultAWSCredentialsProviderChain {
-		
+
 	fn creds_from_env() -> Result<AWSCredentials, VarError> {
 		let env_key = try!(var("AWS_ACCESS_KEY_ID"));
 		let env_secret = try!(var("AWS_SECRET_KEY"));
 
-		Ok(AWSCredentials { key: env_key, secret: env_secret }) 
+		Ok(AWSCredentials { key: env_key, secret: env_secret })
 	}
-	/*
+
 	fn creds_from_profile() -> Result<AWSCredentials, ProfileCredentialsError> {
-			
-		let file = match File::open("test_file.txt") {
-			Ok(file) => file,
-			Err(..) => panic!("room")
-		};
-		
-		let reader = BufReader::new(&file);
-		for line in reader.lines() {
-			let _s = line.unwrap();
-						
-		}
-	
-		Err(ProfileCredentialsError)
-	
+        let path = Path::new("sample-credentials");
+        let display = path.display();
+
+        let mut file = match File::open(&path) {
+            Err(why) => panic!("couldn't open {}: {}", display,
+                                                       Error::description(&why)),
+            Ok(file) => file,
+        };
+
+        let mut contents = String::new();
+        match file.read_to_string(&mut contents) {
+            Err(why) => panic!("couldn't read {}: {}", display,
+                                                       Error::description(&why)),
+            Ok(_) => {},
+        }
+
+        let profile_key = String::from("foo");
+        let secret_key = String::from("bar");
+
+        return  Ok(AWSCredentials{ key: profile_key, secret: secret_key });
+
+		// Err(ProfileCredentialsError)
+
 	}
-	*/
+
+    // IAM role
 }
