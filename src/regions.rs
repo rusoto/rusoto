0ce1c526b66d22ed133ff79c39f1270088a99758
Matthew Mayer <matthewkmayer@gmail.com>
2015-08-20 12:22:44 -0700

    Unifies region_in_aws_format returns in matcher.

--- src/regions.rs
+++ src/regions.rs
@@ -15,13 +15,13 @@ pub fn region_in_aws_format(region: &Region) -> String {
     match region {
         &Region::UsEast1 => "us-east-1".to_string(),
         &Region::UsWest1 => "us-west-1".to_string(),
-        &Region::UsWest2 => return "us-west-2".to_string(),
-        &Region::EuWest1 => return "eu-west-1".to_string(),
-        &Region::EuCentral1 => return "eu-central-1".to_string(),
-        &Region::ApSoutheast1 => return "ap-southeast-1".to_string(),
-        &Region::ApNortheast1 => return "ap-northeast-1".to_string(),
-        &Region::ApSoutheast2 => return "ap-southeast-2".to_string(),
-        &Region::SaEast1 => return "sa-east-1".to_string(),
+        &Region::UsWest2 => "us-west-2".to_string(),
+        &Region::EuWest1 => "eu-west-1".to_string(),
+        &Region::EuCentral1 => "eu-central-1".to_string(),
+        &Region::ApSoutheast1 => "ap-southeast-1".to_string(),
+        &Region::ApNortheast1 => "ap-northeast-1".to_string(),
+        &Region::ApSoutheast2 => "ap-southeast-2".to_string(),
+        &Region::SaEast1 => "sa-east-1".to_string(),
     }
 }
 
