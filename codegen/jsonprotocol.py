4c3c376810496ea8696032b0657fb7334ff1dc95
Anthony DiMarco <ocramida@gmail.com>
2015-12-20 18:52:19 -0800

    refactoring botocore_parser to polymorphic class structure

---
+++ codegen/jsonprotocol.py
@@ -0,0 +1,8 @@
+from parserbase import ParserBase
+
+class JsonProtocolParser(ParserBase):
+    """
+    Implementation of ParserBase for AWS services with protocol type 'json'
+    """
+    def request_method(self, operation):
+        pass
--- codegen/sqs.rs
+++ codegen/sqs.rs
@@ -3199,3 +3199,4 @@ impl<'a> SQSClient<'a> {
 		}
 	}
 }
+
