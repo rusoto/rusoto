d410d515dc15d61dca557f5527e4088e287ca4ec
Anthony DiMarco <ocramida@gmail.com>
2016-01-06 21:01:18 -0800

    refactors build.rs to generate code from a Vec<Service> for easier expansion to lots of services later

--- build.rs
+++ build.rs
@@ -10,33 +10,33 @@ use std::process::Command;
 use serde_codegen::register;
 use syntex::Registry;
 
+const BOTOCORE_DIR: &'static str = "codegen/botocore/botocore/data/";
+
+struct Service {
+    name: String,
+    type_name: String, 
+    protocol_date: String
+}
+
 fn main() {
     let out_dir = var_os("OUT_DIR").expect("OUT_DIR not specified");
     let out_path = Path::new(&out_dir);
 
-    // DynamoDB
-    generate(
-        "codegen/botocore/botocore/data/dynamodb/2012-08-10/service-2.json",
-        "DynamoDBClient",
-        out_path,
-        "dynamodb",
-    );
+    let botocore_dir = var_os("BOTOCORE_DIR");
+    let botocore_path = match botocore_dir {
+        Some(ref dirname) => Path::new(dirname),
+        None => Path::new(BOTOCORE_DIR)
+    };
+    
+    let services = vec![
+        Service::new("dynamodb", "DynamoDBClient", "2012-08-10"),
+        Service::new("kms", "KMSClient", "2014-11-01"),
+        Service::new("sqs", "SQSClient", "2012-11-05")
+    ];
 
-    // KMS
-    generate(
-        "codegen/botocore/botocore/data/kms/2014-11-01/service-2.json",
-        "KMSClient",
-        out_path,
-        "kms",
-    );
-
-    // SQS
-    generate(
-        "codegen/botocore/botocore/data/sqs/2012-11-05/service-2.json",
-        "SQSClient",
-        out_path,
-        "sqs",
-    )
+    for service in services {        
+        generate(service, botocore_path, out_path);
+    }
 }
 
 fn botocore_generate(input: &str, type_name: &str, destination: &Path) {
@@ -56,15 +56,17 @@ fn botocore_generate(input: &str, type_name: &str, destination: &Path) {
 }
 
 fn generate(
-    input: &str,
-    type_name: &str,
+    service: Service,
+    botocore_path: &Path,
     base_destination: &Path,
-    service_name: &str,
 ) {
-    let botocore_destination = base_destination.join(format!("{}_botocore.rs", service_name));
-    let serde_destination = base_destination.join(format!("{}.rs", service_name));
+    let botocore_destination = base_destination.join(format!("{}_botocore.rs", service.name));
+    let serde_destination = base_destination.join(format!("{}.rs", service.name));
+    let input_location = botocore_path.join(format!("{}/{}/service-2.json", service.name, service.protocol_date));
 
-    botocore_generate(input, type_name, botocore_destination.as_path());
+    let input = input_location.to_str().expect(&format!("Invalid service definition path for {} {}", service.protocol_date, service.name));
+
+    botocore_generate(input, &service.type_name, botocore_destination.as_path());
     serde_generate(botocore_destination.as_path(), serde_destination.as_path());
 }
 
@@ -73,4 +75,10 @@ fn serde_generate(source: &Path, destination: &Path) {
 
     register(&mut registry);
     registry.expand("", source, destination).expect("failed to generate code with Serde");
+}
+
+impl Service {
+    fn new<S: ToString>(name: S, type_name: S, protocol_date: S) -> Service {
+        Service { name: name.to_string(), type_name: type_name.to_string(), protocol_date: protocol_date.to_string() }
+    }
 }
