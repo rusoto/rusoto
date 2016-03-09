498144321debbac6078cca26902ddc6ea7b38b64
Jimmy Cuadra <jimmy@jimmycuadra.com>
2016-03-09 00:02:21 -0800

    Remove "get_" prefix from getter methods.

--- src/request.rs
+++ src/request.rs
@@ -12,7 +12,7 @@ use log::LogLevel::Debug;
 
 /// Takes a fully formed and signed request and executes it.
 pub fn send_request(signed_request: &SignedRequest) -> Response {
-    let hyper_method = match signed_request.get_method().as_ref() {
+    let hyper_method = match signed_request.method().as_ref() {
         "POST" => Method::Post,
         "PUT" => Method::Put,
         "DELETE" => Method::Delete,
@@ -22,18 +22,18 @@ pub fn send_request(signed_request: &SignedRequest) -> Response {
 
     // translate the headers map to a format Hyper likes
     let mut hyper_headers = Headers::new();
-    for h in signed_request.get_headers().iter() {
+    for h in signed_request.headers().iter() {
         hyper_headers.set_raw(h.0.to_owned(), h.1.to_owned());
     }
 
-    let mut final_uri = format!("https://{}{}", signed_request.get_hostname(), signed_request.get_canonical_uri());
-    if signed_request.get_canonical_query_string().len() > 0 {
-        final_uri = final_uri + &format!("?{}", signed_request.get_canonical_query_string());
+    let mut final_uri = format!("https://{}{}", signed_request.hostname(), signed_request.canonical_uri());
+    if signed_request.canonical_query_string().len() > 0 {
+        final_uri = final_uri + &format!("?{}", signed_request.canonical_query_string());
     }
 
     if log_enabled!(Debug) {
         debug!("Full request: \n method: {}\n final_uri: {}\n payload: {:?}\nHeaders:\n",
-        	hyper_method, final_uri, signed_request.get_payload());
+            hyper_method, final_uri, signed_request.payload());
         for h in hyper_headers.iter() {
             debug!("{}:{}", h.name(), h.value_string());
         }
@@ -42,7 +42,7 @@ pub fn send_request(signed_request: &SignedRequest) -> Response {
     let mut client = Client::new();
     client.set_redirect_policy(RedirectPolicy::FollowNone);
 
-    match signed_request.get_payload() {
+    match signed_request.payload() {
         None => client.request(hyper_method, &final_uri).headers(hyper_headers).body("").send().unwrap(),
         Some(payload_contents) => client.request(hyper_method, &final_uri).headers(hyper_headers).body(payload_contents).send().unwrap(),
     }
