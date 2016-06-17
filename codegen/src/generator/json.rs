6f1ba2f358ef7fa465ecd889ae28face61cb94af
Anthony DiMarco <ocramida@gmail.com>
2016-06-16 22:28:46 -0700

    don't add a Validation type to the error enum if the protocol being generated alredy defines one

--- codegen/src/generator/json.rs
+++ codegen/src/generator/json.rs
@@ -150,16 +150,26 @@ pub fn generate_error_type(operation: &Operation, error_documentation: &HashMap<
 
 fn generate_error_enum_types(operation: &Operation, error_documentation: &HashMap<&String, &String>) -> Option<String> {
     let mut enum_types: Vec<String> = Vec::new();
+    let mut add_validation = true;
 
     if operation.errors.is_some() {
         for error in operation.errors.as_ref().unwrap().iter() {
+            let error_name = error.idiomatic_error_name();
+
             enum_types.push(format!("\n///{}\n{}(String)",
                 error_documentation.get(&error.shape).unwrap_or(&&String::from("")),
-                error.idiomatic_error_name()));
+                error_name));
+
+            if error_name == "Validation" {
+                add_validation = false;
+            }
         }
     }
 
-    enum_types.push("/// A validation error occurred.  Details from AWS are provided.\nValidation(String)".to_string());
+    if add_validation {
+        enum_types.push("/// A validation error occurred.  Details from AWS are provided.\nValidation(String)".to_string());
+    }
+
     enum_types.push("/// An unknown error occurred.  The raw HTTP response is provided.\nUnknown(String)".to_string());
     Some(enum_types.join(","))
 }
@@ -167,36 +177,57 @@ fn generate_error_enum_types(operation: &Operation, error_documentation: &HashMa
 fn generate_error_type_matchers(operation: &Operation) -> Option<String> {
     let mut type_matchers: Vec<String> = Vec::new();
     let error_type = operation.error_type_name();
+    let mut add_validation = true;
 
     if operation.errors.is_some() {
         for error in operation.errors.as_ref().unwrap().iter() {
+            let error_name = error.idiomatic_error_name();
+
             type_matchers.push(format!("\"{error_shape}\" => {error_type}::{error_name}(String::from(body))",
                 error_shape = error.shape,
                 error_type = error_type,
-                error_name = error.idiomatic_error_name()))
+                error_name = error_name));
+
+            if error_name == "Validation" {
+                add_validation = false;
+            }
+
         }
     }
 
-   type_matchers.push(format!("\"Validation\" => {error_type}::Validation(String::from(body))", error_type = error_type));
-   type_matchers.push(format!("_ => {error_type}::Unknown(String::from(body))",  error_type = error_type));
-   Some(type_matchers.join(","))
+    if add_validation {
+        type_matchers.push(format!("\"Validation\" => {error_type}::Validation(String::from(body))", error_type = error_type));
+    }
+
+    type_matchers.push(format!("_ => {error_type}::Unknown(String::from(body))",  error_type = error_type));
+    Some(type_matchers.join(","))
 }
 
 fn generate_error_description_matchers(operation: &Operation) -> Option<String> {
     let mut type_matchers: Vec<String> = Vec::new();
     let error_type = operation.error_type_name();
+    let mut add_validation = true;
 
     if operation.errors.is_some() {
         for error in operation.errors.as_ref().unwrap().iter() {
+            let error_name = error.idiomatic_error_name();
             type_matchers.push(format!("{error_type}::{error_shape}(ref cause) => cause",
                 error_type = operation.error_type_name(),
-                error_shape = error.idiomatic_error_name()))
+                error_shape = error_name));
+
+            if error_name == "Validation" {
+                add_validation = false;
+            }
+
         }
     }
 
-   type_matchers.push(format!("{error_type}::Validation(ref cause) => cause", error_type = error_type));
-   type_matchers.push(format!("{error_type}::Unknown(ref cause) => cause", error_type = error_type));
-   Some(type_matchers.join(","))
+    if add_validation {
+        type_matchers.push(format!("{error_type}::Validation(ref cause) => cause", error_type = error_type));
+    }
+
+    type_matchers.push(format!("{error_type}::Unknown(ref cause) => cause", error_type = error_type));
+    Some(type_matchers.join(","))
 }
 
 fn generate_result_type<'a>(service: &Service, operation: &Operation, output_type: &'a str) -> String {
