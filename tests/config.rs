8b45e66368943cf3133f2a8d9a1ec4cca1b747d8
Anthony DiMarco <ocramida@gmail.com>
2016-06-25 14:39:25 -0700

    add AWS Config integration tests

---
+++ tests/config.rs
@@ -0,0 +1,38 @@
+#![cfg(feature = "config")]
+
+extern crate rusoto;
+
+use rusoto::config::{ConfigServiceClient, DescribeConfigRulesRequest, DescribeDeliveryChannelsRequest};
+use rusoto::{DefaultCredentialsProvider, Region};
+
+#[test]
+fn should_describe_config_rules() {
+    let credentials = DefaultCredentialsProvider::new().unwrap();
+    let client = ConfigServiceClient::new(credentials, Region::UsEast1);
+
+    let request = DescribeConfigRulesRequest::default();
+
+    match client.describe_config_rules(&request) {
+        Ok(response) => {
+            println!("{:#?}", response); 
+            assert!(true)
+        },
+        Err(err) => panic!("Expected OK response, got {:#?}", err)
+    };
+}
+
+#[test]
+fn should_describe_delivery_channels() {
+    let credentials = DefaultCredentialsProvider::new().unwrap();
+    let client = ConfigServiceClient::new(credentials, Region::UsEast1);
+
+    let request = DescribeDeliveryChannelsRequest::default();
+
+    match client.describe_delivery_channels(&request) {
+        Ok(response) => {
+            println!("{:#?}", response); 
+            assert!(true)
+        },
+        Err(err) => panic!("Expected OK response, got {:#?}", err)
+    };
+}
