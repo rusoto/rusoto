ce7a7b6da313131f7d8eb2beeedf9cf858b44602
Anthony DiMarco <ocramida@gmail.com>
2016-06-25 10:32:29 -0700

    add ACM integration tests skeleton

---
+++ tests/acm.rs
@@ -0,0 +1,25 @@
+#![cfg(feature = "acm")]
+
+extern crate rusoto;
+
+use rusoto::acm::{AcmClient, ListCertificatesRequest};
+use rusoto::{DefaultCredentialsProvider, Region};
+
+#[test]
+fn should_list_certificates_successfully() {
+    let credentials = DefaultCredentialsProvider::new().unwrap();
+    let client = AcmClient::new(credentials, Region::UsEast1);
+
+    let request = ListCertificatesRequest::default();
+
+    match client.list_certificates(&request) {
+    	Ok(response) => {
+    		println!("{:#?}", response); 
+    		assert!(true)
+    	},
+    	Err(err) => panic!("Expected OK response, got {:#?}", err)
+    };
+
+}
+
+
