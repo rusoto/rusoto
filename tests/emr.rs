fbe3e594224a19e090b0d129da86e321d7bb13a9
Anthony DiMarco <ocramida@gmail.com>
2016-06-25 17:37:28 -0700

    add EMR integration tests

---
+++ tests/emr.rs
@@ -0,0 +1,36 @@
+#![cfg(feature = "emr")]
+
+extern crate rusoto;
+
+use rusoto::emr::{EmrClient, ListClustersInput, DescribeJobFlowsInput, DescribeJobFlowsError};
+use rusoto::{DefaultCredentialsProvider, Region};
+
+#[test]
+fn should_list_clusters() {
+    let credentials = DefaultCredentialsProvider::new().unwrap();
+    let client = EmrClient::new(credentials, Region::UsEast1);
+
+    let request = ListClustersInput::default();
+
+    match client.list_clusters(&request) {
+        Ok(response) => {
+            println!("{:#?}", response); 
+            assert!(true)
+        },
+        Err(err) => panic!("Expected OK response, got {:#?}", err)
+    };
+}
+
+#[test]
+fn should_handle_deprecation_gracefully() {
+    let credentials = DefaultCredentialsProvider::new().unwrap();
+    let client = EmrClient::new(credentials, Region::UsEast1);
+
+    let request = DescribeJobFlowsInput::default();
+
+    match client.describe_job_flows(&request) {
+        Err(DescribeJobFlowsError::Validation(msg)) => assert!(msg.contains("DescribeJobFlows API is deprecated.")),
+        err @ _ => panic!("Expected OK response, got {:#?}", err)
+    };
+}
+
