80d31f49a1f3908406ebf72787fa95ebdb8f6d67
Anthony DiMarco <ocramida@gmail.com>
2016-08-16 07:24:08 -0700

    run iot tests through rustfmt

--- tests/iot.rs
+++ tests/iot.rs
@@ -2,13 +2,14 @@
 
 extern crate rusoto;
 extern crate env_logger;
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 use rusoto::iot::{IotClient, ListThingsRequest};
 use rusoto::{DefaultCredentialsProvider, Region};
 
 #[test]
 fn should_list_things() {
-	let _ = env_logger::init();
+    let _ = env_logger::init();
     let credentials = DefaultCredentialsProvider::new().unwrap();
     let client = IotClient::new(credentials, Region::UsEast1);
     let request = ListThingsRequest::default();
