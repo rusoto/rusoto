941b0deeed233dd4de2bb531ea387a3921c17144
Chris Dawes <cmsd2@cantab.net>
2016-09-06 23:03:05 +0100

    tests(SQS): add simple SQS test
    Add simple SQS test for the `list_queues` function.

---
+++ tests/sqs.rs
@@ -0,0 +1,20 @@
+#![cfg(feature = "sqs")]
+
+extern crate rusoto;
+
+use rusoto::sqs::SqsClient;
+use rusoto::sqs::ListQueuesRequest;
+use rusoto::{DefaultCredentialsProvider, Region};
+
+#[test]
+fn list_queues() {
+    let credentials = DefaultCredentialsProvider::new().unwrap();
+
+    let sqs = SqsClient::new(credentials, Region::EuWest1);
+
+    // http://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/Welcome.html
+    let request = ListQueuesRequest {
+        ..Default::default()
+    };
+    sqs.list_queues(&request).unwrap();
+}
