6a32087d714f5aefcfae7db43d3c4d8302a16766
Matthew Mayer <matthewkmayer@gmail.com>
2016-09-06 21:16:58 -0700

    Use botocore defs for SQS mock request example.

--- src/sqs.rs
+++ src/sqs.rs
@@ -6,6 +6,9 @@ include!(concat!(env!("OUT_DIR"), "/sqs.rs"));
 
 #[cfg(test)]
 mod test {
+	use std::fs::File;
+	use std::io::Read;
+
 	use sqs::{SqsClient, CreateQueueRequest};
     use super::super::{Region, SignedRequest};
     use super::super::mock::*;
@@ -15,18 +18,19 @@ mod test {
     #[test]
     // sample response from the SQS documentation
     fn should_parse_example_create_queue_response() {
+		let sample_creation_response_location = "./codegen/botocore/tests/unit/response_parsing/xml/responses/sqs-create-queue.xml";
+		let mut input_file = File::open(sample_creation_response_location)
+			.expect("couldn't find file");
+
+	    let mut mock_response = String::new();
+
+	    input_file.read_to_string(&mut mock_response).expect(&format!(
+	        "Failed to read {:?}",
+	        sample_creation_response_location,
+	    ));
+
         let mock = MockRequestDispatcher::with_status(200)
-            .with_body(r#"
-				<?xml version="1.0" encoding="UTF-8"?>
-				<CreateQueueResponse>
-					<CreateQueueResult>
-						<QueueUrl>http://queue.amazonaws.com/123456789012/testQueue</QueueUrl>
-					</CreateQueueResult>
-					<ResponseMetadata>
-						<RequestId>7a62c49f-347e-4fc4-9331-6e8e7a96aa73</RequestId>
-					</ResponseMetadata>
-				</CreateQueueResponse>
-            "#)
+            .with_body(&mock_response)
             .with_request_checker(|request: &SignedRequest| {
                 assert_eq!(request.method, "POST");
                 assert_eq!(request.path, "/");
@@ -41,6 +45,6 @@ mod test {
         request.queue_name = "testQueue".to_string();
 
         let result = client.create_queue(&request).unwrap();
-        assert_eq!(result.queue_url, Some("http://queue.amazonaws.com/123456789012/testQueue".to_string()));
+        assert_eq!(result.queue_url, Some("http://sqs.us-east-1.amazonaws.com/123456789012/testQueue".to_string()));
     }
 }
