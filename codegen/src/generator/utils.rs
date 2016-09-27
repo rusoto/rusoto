7b547637d45735710f5856c2e0e90d946d10710c
David Eagen <david@eagen.com>
2016-09-27 03:40:57 -0500

    Replace capitalize_first_letter_of_string and use the existing capitalize_first instead.

--- codegen/src/generator/utils.rs
+++
@@ -1,8 +0,0 @@
-/// Takes a string and returns it with the first letter capitalized.
-pub fn capitalize_first_letter_of_string(s: &str) -> String {
-    let mut chars = s.chars();
-    match chars.next() {
-        Some(c) => c.to_uppercase().collect::<String>() + chars.as_str(),
-        None => String::new(),
-    }
-}
