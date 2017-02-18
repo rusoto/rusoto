64538e90e721ad91c02c456e8609b5d4d2c7498f
Matthew Mayer <matthew.mayer@ionchannel.io>
2017-02-18 09:31:18 -0800

    Remove use of feature flag in credentials that we don't use any more.

--- credential/src/lib.rs
+++ credential/src/lib.rs
@@ -1,6 +1,5 @@
 #![cfg_attr(feature = "nightly-testing", feature(plugin))]
 #![cfg_attr(feature = "nightly-testing", plugin(clippy))]
-#![cfg_attr(not(feature = "unstable"), deny(warnings))]
 
 //! Types for loading and managing AWS access credentials for API requests.
 
