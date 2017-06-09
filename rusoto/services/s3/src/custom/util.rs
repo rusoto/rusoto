c103d9f99f4544c839a5d61aed32a7542e58a703
Matt Jones <mthjones@gmail.com>
2017-06-09 01:43:43 -0400

    Improve encode_key docs

--- rusoto/services/s3/src/custom/util.rs
+++ rusoto/services/s3/src/custom/util.rs
@@ -1,22 +1,19 @@
 use rusoto_core::signature;
 
 /// URL encodes an S3 object key. This is necessary for `copy_object` and `upload_part_copy`,
-/// which require the `copy_source` field to be URL encoded to be used properly.
+/// which require the `copy_source` field to be URL encoded.
 ///
 /// # Examples
 ///
 /// ```
-/// # use rusoto_s3::{S3, CopyObjectRequest};
-/// # fn test_fn<T: S3>(s3_client: T) {
-///
-/// let response = s3_client.copy_object(&CopyObjectRequest {
+/// use rusoto_s3::CopyObjectRequest;
+/// 
+/// let request = CopyObjectRequest {
 ///     bucket: "my-bucket".to_owned(),
 ///     key: "my-key".to_owned(),
 ///     copy_source: rusoto_s3::util::encode_key("other-buckét/key-to-cöpy"),
 ///     ..Default::default()
-/// }).unwrap();
-///
-/// # }
+/// };
 pub fn encode_key<T: AsRef<str>>(key: T) -> String {
     signature::encode_uri_path(key.as_ref())
 }
