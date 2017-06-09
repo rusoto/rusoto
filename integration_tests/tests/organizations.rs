6ded1f4b376c9e1eab21bf82a2379ac2cf1a7048
Dru Sellers <dru@drusellers.com>
2017-06-08 19:11:21 -0500

    Add organization test

---
+++ integration_tests/tests/organizations.rs
@@ -0,0 +1,18 @@
+#![cfg(feature = "organizations")]
+extern crate env_logger;
+extern crate rusoto_core;
+extern crate rusoto_organizations;
+
+use rusoto_organizations::{Organizations, OrganizationsClient, DescribeOrganizations};
+use rusoto_core::{DefaultCredentialsProvider, Region};
+use rusoto_core::default_tls_client;
+
+#[test]
+fn should_describe_organizations() {
+    let _ = env_logger::init();
+    let credentials = DefaultCredentialsProvider::new().unwrap();
+    let client = OrganizationsClient::new(default_tls_client().unwrap(), credentials, Region::UsEast1);
+
+    let result = client.describe_organization();
+    println!("{:#?}", result);
+}
