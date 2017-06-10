acbe0356f1b577b83ca3addc434a3e49a8f0b90b
Matt Jones <mthjones@gmail.com>
2017-06-09 22:22:30 -0400

    Add integration test for StepFunctions service

---
+++ integration_tests/tests/stepfunctions.rs
@@ -0,0 +1,18 @@
+#![cfg(feature = "stepfunctions")]
+
+extern crate rusoto_core;
+extern crate rusoto_stepfunctions;
+
+use rusoto_stepfunctions::{StepFunctions, StepFunctionsClient, ListStateMachinesInput};
+use rusoto_core::{DefaultCredentialsProvider, Region};
+use rusoto_core::default_tls_client;
+
+#[test]
+fn should_list_state_machines() {
+    let credentials = DefaultCredentialsProvider::new().unwrap();
+    let client = StepFunctionsClient::new(default_tls_client().unwrap(), credentials, Region::UsEast1);
+    let request = ListStateMachinesInput::default();
+
+    let result = client.list_state_machines(&request).unwrap();
+    println!("{:#?}", result);
+}
