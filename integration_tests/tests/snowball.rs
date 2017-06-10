3ba5615e830fc75aa76830b8b846f39442132a3f
Matt Jones <mthjones@gmail.com>
2017-06-09 22:34:49 -0400

    Add integration tests for Snowball

---
+++ integration_tests/tests/snowball.rs
@@ -0,0 +1,18 @@
+#![cfg(feature = "snowball")]
+
+extern crate rusoto_core;
+extern crate rusoto_snowball;
+
+use rusoto_snowball::{Snowball, SnowballClient, DescribeAddressesRequest};
+use rusoto_core::{DefaultCredentialsProvider, Region};
+use rusoto_core::default_tls_client;
+
+#[test]
+fn should_describe_addresses() {
+    let credentials = DefaultCredentialsProvider::new().unwrap();
+    let client = SnowballClient::new(default_tls_client().unwrap(), credentials, Region::UsEast1);
+    let request = DescribeAddressesRequest::default();
+
+    let result = client.describe_addresses(&request).unwrap();
+    println!("{:#?}", result);
+}
