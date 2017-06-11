f0574f5cb1e7aa08d9c2dedc5c414bb2bf5647d9
Matt Jones <mthjones@gmail.com>
2017-06-11 14:31:23 -0400

    Add integration tests for AWS Server Migration Service

---
+++ integration_tests/tests/sms.rs
@@ -0,0 +1,16 @@
+#[cfg(feature = "sms")]
+
+extern crate rusoto_core;
+extern crate rusoto_sms;
+
+use rusoto_sms::{ServerMigrationService, ServerMigrationServiceClient, GetServersRequest};
+use rusoto_core::{default_tls_client, DefaultCredentialsProvider, Region};
+
+#[test]
+fn should_get_servers() {
+    let provider = DefaultCredentialsProvider::new().unwrap();
+    let client = ServerMigrationServiceClient::new(default_tls_client().unwrap(), provider, Region::UsEast1);
+
+    let response = client.get_servers(&GetServersRequest::default()).unwrap();
+    println!("{:#?}", response);
+}
