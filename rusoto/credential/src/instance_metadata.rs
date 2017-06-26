9f2fb7c425d14fef2fffed06bc9080d256713068
Thomas HUDRY <thomas.hudry@gmail.com>
2017-06-26 23:49:04 +0100

    Remove retry for credentials providers

--- rusoto/credential/src/instance_metadata.rs
+++ rusoto/credential/src/instance_metadata.rs
@@ -1,7 +1,6 @@
 //! The Credentials Provider for an AWS Resource's IAM Role.
 
 use reqwest;
-use retry::retry_exponentially;
 use std::io::Read;
 
 use {AwsCredentials, CredentialsError, ProvideAwsCredentials, parse_credentials_from_aws_service};
@@ -21,59 +20,72 @@ impl ProvideAwsCredentials for InstanceMetadataProvider {
 
 /// Gets the role name to get credentials for using the IAM Metadata Service (169.254.169.254).
 fn get_role_name() -> Result<String, CredentialsError> {
-    let role_name_address = format!("http://{}/{}", AWS_CREDENTIALS_PROVIDER_IP, AWS_CREDENTIALS_PROVIDER_PATH);
-    let mut response =
-        match retry_exponentially(5, 7f64, || { reqwest::get(&role_name_address) },
-        |response_attempt| {
-            match response_attempt.as_ref() {
-                Ok(response_returned) => response_returned.status().is_success(),
-                Err(_) => false,
-            }
-        })
-    {
-        Ok(response_from_try) => {
-            match response_from_try {
-                Ok(resp) => resp,
-                Err(err) => return Err(CredentialsError::new(format!("Couldn't connect to credentials role provider: {}", err))),
+    let role_name_address = format!(
+        "http://{}/{}",
+        AWS_CREDENTIALS_PROVIDER_IP,
+        AWS_CREDENTIALS_PROVIDER_PATH
+    );
+    let response = reqwest::get(&role_name_address);
+
+    match response {
+        Ok(mut resp) => {
+            if resp.status().is_success() {
+                let mut role_name = String::new();
+                if resp.read_to_string(&mut role_name).is_err() {
+                    return Err(CredentialsError::new(
+                        "Didn't get a parsable role name from metadata service",
+                    ));
+                }
+                Ok(role_name)
+            } else {
+                return Err(CredentialsError::new(format!(
+                    "Couldn't connect to credentials provider: {}",
+                    resp.status()
+                )));
             }
         }
-        Err(error) => return Err(CredentialsError::new(format!("Couldn't connect to credentials role provider: {}", error))),
-    };
-
-    let mut role_name = String::new();
-    if response.read_to_string(&mut role_name).is_err() {
-        return Err(CredentialsError::new("Didn't get a parsable role name from metadata service"));
+        Err(err) => {
+            return Err(CredentialsError::new(
+                format!("Couldn't connect to credentials provider: {}", err),
+            ))
+        }
     }
-
-    Ok(role_name)
 }
 
 /// Gets the credentials for an EC2 Instances IAM Role.
 fn get_credentials_from_role() -> Result<AwsCredentials, CredentialsError> {
     let role_name = try!(get_role_name());
-    let credentials_provider_url = format!("http://{}/{}/{}", AWS_CREDENTIALS_PROVIDER_IP, AWS_CREDENTIALS_PROVIDER_PATH, role_name);
-    let mut response =
-        match retry_exponentially(5, 7f64, || { reqwest::get(&credentials_provider_url) },
-        |response_attempt| {
-            match response_attempt.as_ref() {
-                Ok(response_returned) => response_returned.status().is_success(),
-                Err(_) => false,
-            }
-        })
-    {
-        Ok(response_from_try) => {
-            match response_from_try {
-                Ok(resp) => resp,
-                Err(err) => return Err(CredentialsError::new(format!("Couldn't connect to credentials provider: {}", err))),
+    let credentials_provider_url = format!(
+        "http://{}/{}/{}",
+        AWS_CREDENTIALS_PROVIDER_IP,
+        AWS_CREDENTIALS_PROVIDER_PATH,
+        role_name
+    );
+
+    let response = reqwest::get(&credentials_provider_url);
+
+    match response {
+        Ok(mut resp) => {
+            if resp.status().is_success() {
+                let mut credentials_response = String::new();
+                if resp.read_to_string(&mut credentials_response).is_err() {
+                    return Err(CredentialsError::new(
+                        "Had issues with reading iam role response: {}",
+                    ));
+                }
+
+                parse_credentials_from_aws_service(&credentials_response)
+            } else {
+                return Err(CredentialsError::new(format!(
+                    "Couldn't connect to credentials provider: {}",
+                    resp.status()
+                )));
             }
         }
-        Err(error) => return Err(CredentialsError::new(format!("Couldn't connect to credentials provider: {}", error))),
-    };
-
-    let mut credentials_response = String::new();
-    if response.read_to_string(&mut credentials_response).is_err() {
-        return Err(CredentialsError::new("Had issues with reading iam role response: {}"));
+        Err(err) => {
+            return Err(CredentialsError::new(
+                format!("Couldn't connect to credentials provider: {}", err),
+            ))
+        }
     }
-
-    parse_credentials_from_aws_service(&credentials_response)
 }
