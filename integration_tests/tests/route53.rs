f0da47e2c9f548eaa563e58a8a9b07292b61b585
Matthew Mayer <matthewkmayer@gmail.com>
2017-08-16 22:19:22 -0700

    Adds integration test for route53.

---
+++ integration_tests/tests/route53.rs
@@ -0,0 +1,17 @@
+#![cfg(feature = "route53")]
+
+extern crate rusoto_core;
+extern crate rusoto_route53;
+
+use rusoto_route53::{Route53, Route53Client, ListHostedZonesRequest};
+use rusoto_core::{DefaultCredentialsProvider, Region, default_tls_client};
+
+#[test]
+fn should_list_hosted_zones() {
+    let credentials = DefaultCredentialsProvider::new().unwrap();
+    let client =
+        Route53Client::new(default_tls_client().unwrap(), credentials, Region::UsEast1);
+    let request = ListHostedZonesRequest::default();
+
+    client.list_hosted_zones(&request).unwrap();
+}
