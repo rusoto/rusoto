e494b7ddab1e4905aa0337067aeb33cea6731a29
Anthony DiMarco <ocramida@gmail.com>
2017-08-28 13:43:19 -0400

    add application autoscaling

---
+++ integration_tests/tests/application-autocaling.rs
@@ -0,0 +1,21 @@
+#![cfg(feature = "application-autoscaling")]
+
+extern crate rusoto_core;
+extern crate rusoto_application_autoscaling;
+
+use rusoto_application_autoscaling::{ApplicationAutoScaling, ApplicationAutoScalingClient, DescribeScalingPoliciesRequest};
+use rusoto_core::{DefaultCredentialsProvider, Region};
+use rusoto_core::default_tls_client;
+
+#[test]
+fn should_describe_scaling_policies() {
+    let credentials = DefaultCredentialsProvider::new().unwrap();
+    let client = ApplicationAutoScalingClient::new(default_tls_client().unwrap(), credentials, Region::UsEast1);
+
+    let request = DescribeScalingPoliciesRequest{
+        service_namespace: "ec2".to_owned(),
+        ..Default::default()
+    };
+
+    client.describe_scaling_policies(&request).unwrap();
+}
