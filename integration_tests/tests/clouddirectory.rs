b67ea5268355c0f1f8037c12fafeaabbf71b5860
Matthew Mayer <matthewkmayer@gmail.com>
2017-10-08 20:04:53 -0700

    Adds Cloud Directory service.

---
+++ integration_tests/tests/clouddirectory.rs
@@ -0,0 +1,19 @@
+#![cfg(feature = "clouddirectory")]
+
+extern crate rusoto_core;
+extern crate rusoto_clouddirectory;
+
+use rusoto_clouddirectory::{CloudDirectory, CloudDirectoryClient, ListDirectoriesRequest};
+use rusoto_core::{DefaultCredentialsProvider, Region};
+use rusoto_core::default_tls_client;
+
+#[test]
+fn should_list_directories() {
+    let client = CloudDirectoryClient::new(default_tls_client().unwrap(),
+                                           DefaultCredentialsProvider::new().unwrap(),
+                                           Region::UsEast1);
+    let request = ListDirectoriesRequest::default();
+
+    let result = client.list_directories(&request).unwrap();
+    println!("{:#?}", result);
+}
