16c6d49d25061b5fdf97211a3dc61b5647df7097
Matthew Mayer <matthewkmayer@gmail.com>
2017-10-18 22:21:57 -0700

    Adds Budget service.

---
+++ rusoto/services/budgets/src/lib.rs
@@ -0,0 +1,30 @@
+
+// =================================================================
+//
+//                           * WARNING *
+//
+//                    This file is generated!
+//
+//  Changes made to this file will be overwritten. If changes are
+//  required to the generated code, the service_crategen project
+//  must be updated to generate the changes.
+//
+// =================================================================
+
+//! All public APIs for AWS Budgets
+//!
+//! If you're using the service, you're probably looking for [BudgetsClient](struct.BudgetsClient.html) and [Budgets](trait.Budgets.html).
+
+extern crate hyper;
+extern crate rusoto_core;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+mod generated;
+mod custom;
+
+pub use generated::*;
+pub use custom::*;
+            
