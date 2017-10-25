309dde03578168f4cdaad2d6aab70ab2b58e1c47
Matthew Mayer <matthewkmayer@gmail.com>
2017-10-24 20:24:09 -0700

    Adds MigrationHub integration test.

---
+++ integration_tests/tests/mgh.rs
@@ -0,0 +1,17 @@
+#![cfg(feature = "mgh")]
+
+extern crate rusoto_core;
+extern crate rusoto_mgh;
+
+use rusoto_core::{DefaultCredentialsProvider, Region, default_tls_client};
+use rusoto_mgh::{MigrationHub, MigrationHubClient, ListMigrationTasksRequest};
+
+#[test]
+fn should_list_migration_tasks() {
+    let credentials = DefaultCredentialsProvider::new().unwrap();
+    let client = MigrationHubClient::new(default_tls_client().unwrap(), credentials, Region::UsWest2);
+    let request = ListMigrationTasksRequest::default();
+
+    let result = client.list_migration_tasks(&request).unwrap();
+    println!("Results: {:?}", result);
+}
