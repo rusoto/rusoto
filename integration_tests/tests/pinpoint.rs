2df405096603f02a71128903cb287d04f0f122e4
Matthew Mayer <matthewkmayer@gmail.com>
2017-10-31 21:42:56 -0700

    REST URI snakeification attempt.

--- integration_tests/tests/pinpoint.rs
+++ integration_tests/tests/pinpoint.rs
@@ -2,16 +2,19 @@
 
 extern crate rusoto_core;
 extern crate rusoto_pinpoint;
+extern crate env_logger;
+extern crate log;
 
-use rusoto_pinpoint::{Pinpoint, PinpointClient, DescribeAppsRequest};
+use rusoto_pinpoint::{Pinpoint, PinpointClient, GetAppsRequest};
 use rusoto_core::{DefaultCredentialsProvider, Region, default_tls_client};
 
 #[test]
 fn should_describe_apps() {
+    let _ = env_logger::init();
     let credentials = DefaultCredentialsProvider::new().unwrap();
     let client = PinpointClient::new(default_tls_client().unwrap(), credentials, Region::UsEast1);
-    let request = DescribeAppsRequest::default();
+    let request = GetAppsRequest::default();
 
-    let result = client.describe_apps(&request).unwrap();
+    let result = client.get_apps(&request).unwrap();
     println!("Result: {:?}", result);
 }
