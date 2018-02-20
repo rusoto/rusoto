e8eb9724ec15da2aaa16580565fc019d4f3ebc79
ramn <github@rymdimperiet.org>
2018-02-20 19:05:58 +0100

    CloudFront ListDistributions parsing workaround
    Due to how the parser is generated and due to what seems like a mismatch between
    the schema in botocore and the actual xml response, we always get an empty list
    of Distributions from this endpoint.
    
    Included is a test to prove it and a very ugly workaround.

---
+++ rusoto/services/cloudfront/src/custom/mod.rs
@@ -0,0 +1,22 @@
+#[cfg(test)]
+mod tests {
+    extern crate rusoto_mock;
+
+    pub use generated::*;
+    use self::rusoto_mock::*;
+    use rusoto_core::Region as rusoto_region;
+
+    #[test]
+    fn test_parse_and_extract_valid_cloudfront_list_distributions() {
+        let mock_response = MockResponseReader::read_response(
+            "test_resources/generated/valid",
+            "cloudfront-list-distributions.xml",
+            );
+        let mock = MockRequestDispatcher::with_status(200).with_body(&mock_response);
+        let client = CloudFrontClient::new(mock, MockCredentialsProvider, rusoto_region::UsEast1);
+        let request = ListDistributionsRequest::default();
+        let result = client.list_distributions(&request).sync();
+        assert!(result.is_ok(), "parse error: {:?}", result);
+        assert!(result.unwrap().distribution_list.is_some(), "Should have a distribution list");
+    }
+}
