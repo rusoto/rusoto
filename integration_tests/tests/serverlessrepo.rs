9385a8b6f9b09d91db4c553e1c79d9715f9ba34b
Matthew Mayer <matthewkmayer@gmail.com>
2018-03-16 23:04:06 -0700

    Add ServerlessRepo service.

---
+++ integration_tests/tests/serverlessrepo.rs
@@ -0,0 +1,16 @@
+#![cfg(feature = "serverlessrepo")]
+extern crate rusoto_core;
+extern crate rusoto_serverlessrepo;
+
+use rusoto_serverlessrepo::{ServerlessRepo, ServerlessRepoClient, ListApplicationsRequest};
+use rusoto_core::Region;
+
+#[test]
+fn should_list_applications() {
+    let client = ServerlessRepoClient::simple(Region::UsEast1);
+    let request = ListApplicationsRequest::default();
+
+    let result = client.list_applications(&request).sync();
+    println!("{:#?}", result);
+    assert!(result.is_ok());
+}
