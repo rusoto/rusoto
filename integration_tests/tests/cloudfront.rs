705317f351ba94857178e951553b1dd01cdcf94b
Matthew Mayer <matthewkmayer@gmail.com>
2018-03-22 20:12:20 -0700

    Correct typo, use env_logger in cloudfront test.

--- integration_tests/tests/cloudfront.rs
+++ integration_tests/tests/cloudfront.rs
@@ -2,12 +2,14 @@
 
 extern crate rusoto_core;
 extern crate rusoto_cloudfront;
+extern crate env_logger;
 
 use rusoto_cloudfront::{CloudFront, CloudFrontClient, ListDistributionsRequest};
 use rusoto_core::Region;
 
 #[test]
-fn should_list_distirbutions() {
+fn should_list_distributions() {
+    let _ = env_logger::try_init();
     let client = CloudFrontClient::simple(Region::UsEast1);
     let request = ListDistributionsRequest::default();
 
