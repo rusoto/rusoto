cc3580cd684a6b2554b41f6ae00f8216dffd7148
softprops <d.tangren@gmail.com>
2018-04-08 20:13:23 -0400

    very basic integration test

---
+++ integration_tests/tests/secretsmanager.rs
@@ -0,0 +1,21 @@
+#![cfg(feature = "secretsmanager")]
+
+extern crate env_logger;
+extern crate rusoto_core;
+extern crate rusoto_secretsmanager;
+
+use rusoto_guardduty::{ListSecretsRequest, SecretsManager, SecretsManagerClient};
+use rusoto_core::Region;
+
+#[test]
+fn should_list_invitations() {
+  let _ = env_logger::try_init();
+  let client = SecretsManagerClient::simple(Region::UsWest2);
+  let request = ListSecretsRequest {
+    ..Default::default()
+  };
+
+  let result = client.list_secrets(&request).sync();
+  println!("{:#?}", result);
+  assert!(result.is_ok());
+}
