83f92f10d13b1435ccf511e4045eb4383b2d828f
Legokichi Duckscallion <legokichi@gmail.com>
2018-10-24 18:30:25 +0900

    add lambda invoke test

--- integration_tests/tests/lambda.rs
+++ integration_tests/tests/lambda.rs
@@ -3,7 +3,7 @@
 extern crate rusoto_core;
 extern crate rusoto_lambda;
 
-use rusoto_lambda::{Lambda, LambdaClient, ListFunctionsRequest};
+use rusoto_lambda::{Lambda, LambdaClient, ListFunctionsRequest, InvocationRequest, InvokeError};
 use rusoto_core::Region;
 
 #[test]
@@ -13,4 +13,41 @@ fn should_list_functions() {
 
     let result = client.list_functions(request).sync().unwrap();
     println!("{:#?}", result);
+}
+
+#[test]
+fn should_function_not_found() {
+    let client = LambdaClient::new(Region::UsEast1);
+    {
+        let request = InvocationRequest{
+            function_name: "no-such-a-function".to_string(),
+            invocation_type: Some("RequestResponse".to_string()),
+            ..Default::default()
+        };
+
+        let result = client.invoke(request).sync();
+
+        assert!(result.is_err());
+        if let Err(InvokeError::Unknown(resp)) = result {
+            assert_eq!(resp.status, 404);
+        }else{
+            assert!(false, format!("expect Err(InvokeError::Unknown(_), found {:?}", result));
+        }
+    }
+    {
+        // ARN with colons
+        let request = InvocationRequest{
+            function_name: "function:no-such-a-function".to_string(),
+            ..Default::default()
+        };
+
+        let result = client.invoke(request).sync();
+
+        assert!(result.is_err());
+        if let Err(InvokeError::Unknown(resp)) = result {
+            assert_eq!(resp.status, 404);
+        }else{
+            assert!(false, format!("expect Err(InvokeError::Unknown(_), found {:?}", result));
+        }
+    }
 }
