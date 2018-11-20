37ea5ac8c6e9974dc9ed8730381a99b474b11a7c
Matthew Mayer <matthewkmayer@gmail.com>
2018-11-19 20:40:13 -0800

    Add IoT Data service.

---
+++ rusoto/services/iot-data/src/lib.rs
@@ -0,0 +1,33 @@
+
+// =================================================================
+//
+//                           * WARNING *
+//
+//                    This file is generated!
+//
+//  Changes made to this file will be overwritten. If changes are
+//  required to the generated code, the service_crategen project
+//  must be updated to generate the changes.
+//
+// =================================================================
+
+#![doc(html_logo_url = "https://raw.githubusercontent.com/rusoto/rusoto/master/assets/logo-square.png")]
+//! <p><fullname>AWS IoT</fullname> <p>AWS IoT-Data enables secure, bi-directional communication between Internet-connected things (such as sensors, actuators, embedded devices, or smart appliances) and the AWS cloud. It implements a broker for applications and things to publish messages over HTTP (Publish) and retrieve, update, and delete thing shadows. A thing shadow is a persistent representation of your things and their state in the AWS cloud.</p></p>
+//!
+//! If you're using the service, you're probably looking for [IotDataClient](struct.IotDataClient.html) and [IotData](trait.IotData.html).
+
+extern crate futures;
+#[macro_use]
+extern crate log;
+extern crate rusoto_core;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+mod generated;
+mod custom;
+
+pub use generated::*;
+pub use custom::*;
+            
