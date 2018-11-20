aad07c709411fbe23cb7304ca0dd57835903a7f4
Will Glynn <will@willglynn.com>
2018-11-19 23:19:44 -0600

    Add some failing AttributeValue serialization tests

---
+++ rusoto/services/dynamodb/src/custom/custom_tests.rs
@@ -0,0 +1,33 @@
+use ::{AttributeValue};
+
+#[test]
+fn attribute_value_default_is_empty() {
+    let all_default = AttributeValue{
+        ..Default::default()
+    };
+
+    let serialized = serde_json::to_string(&all_default).unwrap();
+    assert_eq!(&serialized, "{}");
+}
+
+#[test]
+fn attribute_value_with_blob_contains_only_blob() {
+    let all_default = AttributeValue{
+        b: Some("foo".bytes().collect()),
+        ..Default::default()
+    };
+
+    let serialized = serde_json::to_string(&all_default).unwrap();
+    assert_eq!(&serialized, r#"{"B":"Zm9v"}"#);
+}
+
+#[test]
+fn attribute_value_with_number_contains_only_number() {
+    let all_default = AttributeValue{
+        n: Some(1234.to_string()),
+        ..Default::default()
+    };
+
+    let serialized = serde_json::to_string(&all_default).unwrap();
+    assert_eq!(&serialized, r#"{"N":"1234"}"#);
+}
