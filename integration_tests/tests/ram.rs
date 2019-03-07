8dfa73f7297b085089b3ff0455b86169ee6a3dcf
softprops <d.tangren@gmail.com>
2019-03-07 01:19:30 -0500

    copied test name

--- integration_tests/tests/ram.rs
+++ integration_tests/tests/ram.rs
@@ -7,7 +7,7 @@ use rusoto_core::Region;
 use rusoto_ram::{ListResourcesRequest, Ram, RamClient};
 
 #[test]
-fn should_list_skills() {
+fn should_work() {
     let client = RamClient::new(Region::UsEast1);
     let response = client
         .list_resources(ListResourcesRequest {
