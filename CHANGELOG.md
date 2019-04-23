80c54638563ed24ccece8c111a10c0e41125a6f4
Matthew Mayer <matthewkmayer@gmail.com>
2019-04-22 23:06:21 -0700

    Add DocDB service.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@
 
 - Fix de/serialization of DynamoDB binary set attribute values
 - Change type for blob values from `Vec<u8>` to `Bytes`
+- Add DocDB service
 - Add Kafka service
 
 ## [0.38.0] - 2019-04-17
---
+++ rusoto/services/docdb/src/lib.rs
@@ -0,0 +1,30 @@
+
+// =================================================================
+//
+//                           * WARNING *
+//
+//                    This file is generated!
+//
+//  Changes made to this file will be overwritten. If changes are
+//  required to the generated code, the service_crategen project
+//  must be updated to generate the changes.
+//
+// =================================================================
+
+#![doc(html_logo_url = "https://raw.githubusercontent.com/rusoto/rusoto/master/assets/logo-square.png")]
+//! <p>Amazon DocumentDB API documentation</p>
+//!
+//! If you're using the service, you're probably looking for [DocdbClient](struct.DocdbClient.html) and [Docdb](trait.Docdb.html).
+
+extern crate bytes;
+extern crate futures;
+extern crate rusoto_core;
+extern crate serde_urlencoded;
+extern crate xml;
+
+mod generated;
+mod custom;
+
+pub use generated::*;
+pub use custom::*;
+            
