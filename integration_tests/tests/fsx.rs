9e5e0e792af8fe2865031d3a93b517e14351f9e1
Matthew Mayer <matthewkmayer@gmail.com>
2019-04-23 22:17:11 -0700

    Add FSX service.

---
+++ integration_tests/tests/fsx.rs
@@ -0,0 +1,16 @@
+#![cfg(feature = "fsx")]
+
+extern crate rusoto_core;
+extern crate rusoto_fsx;
+
+use rusoto_core::Region;
+use rusoto_fsx::{Fsx, FsxClient, DescribeFileSystemsRequest};
+
+#[test]
+fn should_describe_filesystems() {
+    let client = FsxClient::new(Region::UsEast1);
+    let request = DescribeFileSystemsRequest::default();
+
+    let res = client.describe_file_systems(request).sync().unwrap();
+    println!("res is {:?}", res);
+}
