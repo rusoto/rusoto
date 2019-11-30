0c8da061f8f3032ce24372683762d08bb3bc9683
Matthew Mayer <matthewkmayer@gmail.com>
2019-11-30 15:45:12 -0800

    Limit publishing rate by reducing parallel compilations.

--- rusoto/services/publish-services.sh
+++ rusoto/services/publish-services.sh
@@ -2,5 +2,7 @@
 
 for D in `find . -maxdepth 1 -mindepth 1 -type d | sort`;
 do
-    (cd $D ; echo "I am in `pwd`" ; cargo publish)
+    # Limit parallelization of compilation to avoid hitting crates.io rate limit.
+    # See https://github.com/rusoto/rusoto/issues/1610 .
+    (cd $D ; echo "I am in `pwd`" ; cargo publish --jobs 4)
 done
