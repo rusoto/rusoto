893d5257ff14cc53b770294e68718816df006215
iliana destroyer of worlds <iweller@amazon.com>
2020-02-07 05:17:59 +0000

    Fix newlines in generated READMEs

--- service_crategen/src/commands/generate/mod.rs
+++ service_crategen/src/commands/generate/mod.rs
@@ -151,33 +151,49 @@ pub fn generate_services(
         writeln!(readme_file, r#"
 # Rusoto {short_name}
 Rust SDK for {aws_name}
+
 You may be looking for:
+
 * [An overview of Rusoto][rusoto-overview]
 * [AWS services supported by Rusoto][supported-aws-services]
 * [API documentation][api-documentation]
 * [Getting help with Rusoto][rusoto-help]
+
 ## Requirements
+
 Rust stable or beta are required to use Rusoto. Nightly is tested, but not guaranteed to be supported. Older
 versions _may_ be supported. The currently supported Rust versions can be found in the Rusoto project
 [`travis.yml`](https://github.com/rusoto/rusoto/blob/master/.travis.yml).
+
 On Linux, OpenSSL is required.
+
 ## Installation
+
 To use `{crate_name}` in your application, add it as a dependency in your `Cargo.toml`:
+
 ```toml
 [dependencies]
 {crate_name} = "{version}"
 ```
+
 ## Crate Features
 - `native-tls` - use platform-specific TLS implementation.
 - `rustls` - use rustls TLS implementation.
 - `serialize_structs` - output structs of most operations get `derive(Serialize)`.
 - `deserialize_structs` - input structs of most operations get `derive(Deserialize)`.
+
 Note: the crate will use the `native-tls` TLS implementation by default.
+
 ## Contributing
+
 See [CONTRIBUTING][contributing].
+
 ## License
+
 Rusoto is distributed under the terms of the MIT license.
+
 See [LICENSE][license] for details.
+
 [api-documentation]: https://docs.rs/{crate_name} "API documentation"
 [license]: https://github.com/rusoto/rusoto/blob/master/LICENSE "MIT License"
 [contributing]: https://github.com/rusoto/rusoto/blob/master/CONTRIBUTING.md "Contributing Guide"
