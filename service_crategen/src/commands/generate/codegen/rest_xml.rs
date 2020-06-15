ca118a489f0620b9c93552fda123cbf9813b35ae
Markus Westerlind <markus.westerlind@distilnetworks.com>
2020-06-15 20:29:01 +0200

    Avoid to_string on String

--- service_crategen/src/commands/generate/codegen/rest_xml.rs
+++ service_crategen/src/commands/generate/codegen/rest_xml.rs
@@ -328,6 +328,7 @@ fn generate_serializer_signature(name: &str) -> String {
 fn generate_primitive_serializer(shape: &Shape) -> String {
     let value_str = match shape.shape_type {
         ShapeType::Blob => "std::str::from_utf8(obj).expect(\"Not a UTF-8 string\")",
+        ShapeType::String | ShapeType::Timestamp => "obj",
         _ => "&obj.to_string()",
     };
     format!(
