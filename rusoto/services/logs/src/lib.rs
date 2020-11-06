f197d33ac2629a96c497998c8cc940f72b4e3e29
alex-berger@gmx.ch <alex-berger@gmx.ch>
2020-11-06 12:46:31 +0100

    Manually fix whitespaces, although id does no make any sense at all. Why are those single space characters in the generated code as generated on the test system but not in the one generated on my machine. I am using the same version for Rust stable and rustfmt as the test does (rustfmt 1.4.20-stable (48f6c32e 2020-08-09)).

--- rusoto/services/logs/src/lib.rs
+++ rusoto/services/logs/src/lib.rs
@@ -26,26 +26,26 @@
 //!
 //! ```rust,no_run
 //! use chrono::Utc;
-//!
+//! 
 //! use rusoto_core::Region;
 //! use rusoto_logs::{
 //!     CloudWatchLogs, CloudWatchLogsClient, DescribeLogStreamsRequest, InputLogEvent,
 //!     PutLogEventsRequest,
 //! };
 //! use std::default::Default;
-//!
+//! 
 //! #[tokio::main]
 //! async fn main() {
 //!     const LOG_GROUP_NAME: &str = "testing";
 //!     const LOG_STREAM_NAME: &str = "testing";
-//!
+//! 
 //!     let client = CloudWatchLogsClient::new(Region::UsEast2);
-//!
+//! 
 //!     let input_log_event = InputLogEvent {
 //!         message: "Test log message".to_string(),
 //!         timestamp: Utc::now().timestamp_millis(), // milliseconds epoch
 //!     };
-//!
+//! 
 //!     // We need the log stream to get the sequence token
 //!     let mut desc_streams_req: DescribeLogStreamsRequest = Default::default();
 //!     desc_streams_req.log_group_name = LOG_GROUP_NAME.to_string();
@@ -56,14 +56,14 @@
 //!         .find(|s| s.log_stream_name == Some(LOG_STREAM_NAME.to_string()))
 //!         .unwrap();
 //!     let sequence_token = stream.upload_sequence_token.clone();
-//!
+//! 
 //!     let put_log_events_request = PutLogEventsRequest {
 //!         log_events: vec![input_log_event], // > 1 must sort by timestamp ASC
 //!         log_group_name: LOG_GROUP_NAME.to_string(),
 //!         log_stream_name: LOG_STREAM_NAME.to_string(),
 //!         sequence_token,
 //!     };
-//!
+//! 
 //!     let resp = client.put_log_events(put_log_events_request).await;
 //!     println!("{:#?}", resp);
 //! }
